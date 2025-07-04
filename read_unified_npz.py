9fc1f75371181e6910236c6333b2811b2fb96efc
Luciaforn <127400456+Luciaforn@users.noreply.github.com>
2025-07-04 23:59:05 +0200

    Update and rename read_npz.py to read_unified_npz.py

---
+++ read_unified_npz.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+data = np.load(r"C:\Users\Lucia\Desktop\Uni\DL\Dataloader\stats_unified_stft_cqt.npz")
+
+print("Chiavi presenti nel file:", data.files)
+
+print("Shape stft_mean:", data["stft_mean"].shape)
+print("stft_mean (preview):\n", data["stft_mean"][0][:10])  
+
+print("Shape cqt_std:", data["cqt_std"].shape)
+print("cqt_std (preview):\n", data["cqt_std"][1][:10])  
